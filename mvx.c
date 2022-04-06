4ba51cb796cca3b24645f0974295c0a7c8859916
nahuel-42 <nievas.nahuel.1998@gmail.com>
2022-04-06 18:30:50 -0300

    Commit Nro2

--- mvx.c
+++ mvx.c
@@ -2,55 +2,54 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#define TR 33
+#define TM 4096 //tamaño memoria
 int ADecimal(char[]);
+void LeeArch(int[]);
+typedef void T_fun(int *,int*);//tipo de funcion
+
+
 int main()
 {
-    int i,instruccion,aux;//instruccion: Instruccion en decimal aux:Copia de instruccion para trabajar
-    char registro[TR];// Char de 32 bytes para sacar de archivo
+    int i,j=0,aux;//instruccion: Instruccion en decimal aux:Copia de instruccion para trabajar
+    int memoria[TM];
+    T_fun* ArrayFunc[]={}; //Arreglo de punteros a funciones, cuando las tengamos las metemos ahi  
+    LeeArch(memoria);
 
-    FILE* arch;
-    arch=fopen("traducido.txt","r");
-    if (arch == NULL)
-        printf("Error en la apertura. Es posible que el archivo no exista");
-    fscanf(arch,"%s",registro);
-    while(!feof(arch)){
-        instruccion=ADecimal(registro);
-        //aux=instruccion>>28;
-        /*if (aux&0xF0000000==0xF0000000){
+    while (j<=6){//falta programar{
+        i=0; 
+        aux=memoria[j];
+        if ((aux&0xF0000000)==0xF0000000){
             i+=16;
-            if(aux&0x0F000000==0x0F000000){
+            if((aux&0x0F000000)==0x0F000000){
                 i+=16;
+                aux=(aux>>20)&0x0000000F;//0 OP
+            }
+            else{
+                aux=(aux>>24)&0x0000000F;//1 OP
             }
         }
         else{
-            i+=ADecimal()
-        }*/
-        printf("%d\n",instruccion);
-        fscanf(arch,"%s",registro);
-    }
+            aux=(aux>>28)&0x0000000F;//2 OP
+        }
+        i+=aux;
+        j++;
+        //ArrayFunc[i-1];    EJECUTOR DE LAS FUNCIONES
+        printf("%d\n",i);
+    }     
     return 0;
 }
 
 
-int ADecimal(char arreglo []){//Transforma char en binario en complemento a 2 a decimal de 32 bits con signo
-    int aux=0,i,N;
-    N=strlen(arreglo)-1;
-    if(arreglo[0]=='1'){
-        for(i=1;i<=N;i++){
-            if(arreglo[i]=='1')
-                arreglo[i]='0';
-            else
-                arreglo[i]='1';
-        }
+void LeeArch(int arreglo[]){
+    FILE* arch;
+    arch=fopen("traducido.mv1","rb");
+    int i=0;
+    if (arch == NULL)
+        printf("Error en la apertura. Es posible que el archivo no exista");
+    fread(arreglo+i,sizeof(int),1,arch);
+    while(!feof(arch)){
+        i++;
+        fread(arreglo+i,sizeof(int),1,arch);
     }
-    for (i=0;i<N;i++){
-        if(arreglo[N-i]=='1')
-            aux+=pow(2,i);
-    }
-    if(arreglo[0]=='1'){
-        aux=aux*-1;
-        aux--;
-    }
-    return aux;
+    fclose(arch);
 }
