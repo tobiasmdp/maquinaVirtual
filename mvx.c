338830e09df08ff277f1600bad93df02d531bf8d
nahuel-42 <nievas.nahuel.1998@gmail.com>
2022-04-09 14:12:21 -0300

    Agregado escaner de operandos

--- mvx.c
+++ mvx.c
@@ -3,53 +3,82 @@
 #include <math.h>
 #include <string.h>
 #define TM 4096 //tamaño memoria
-int ADecimal(char[]);
-void LeeArch(int[]);
+#define TR 16 //tamaño registro
+#define DS 0  //accesos directos de los registros importantes
+#define IP 5
+#define CC 8
+#define AC 9
+#define EXTENDED_MASK 0xFFFFFFFF
+#define REG_MASK 0xFFFF
+#define HIGH_MASK 0xFF00
+#define LOW_MASK 0x00FF
+
+void LeeArch(int[],int[]);
+void ExtraerDosOperandos(int ,int* ,int* );
 typedef void T_fun(int *,int*);//tipo de funcion
 
-
-int main()
-{
-    int i,j=0,aux;//instruccion: Instruccion en decimal aux:Copia de instruccion para trabajar
-    int memoria[TM];
+int main(){
+    int i,aux; //i: numero de operador aux: Almacena la instruccion a ejecutar
+    int A,B;
+    int memoria[TM],registro[TR];
     T_fun* ArrayFunc[]={}; //Arreglo de punteros a funciones, cuando las tengamos las metemos ahi  
-    LeeArch(memoria);
-
-    while (j<=6){//falta programar{
+    LeeArch(memoria,registro);
+    while (registro[IP]<registro[DS]){
         i=0; 
-        aux=memoria[j];
+        aux=memoria[registro[IP]];
+        registro[IP]++;
         if ((aux&0xF0000000)==0xF0000000){
             i+=16;
-            if((aux&0x0F000000)==0x0F000000){
+            if((aux&0x0F000000)==0x0F000000){//0 OP
                 i+=16;
-                aux=(aux>>20)&0x0000000F;//0 OP
+                aux=(aux>>20)&0x0F;
             }
-            else{
-                aux=(aux>>24)&0x0000000F;//1 OP
+            else{//1 OP
+                aux=(aux>>24)&0x00F;
             }
         }
-        else{
-            aux=(aux>>28)&0x0000000F;//2 OP
+        else{//2 OP
+            ExtraerDosOperandos(aux,&A,&B);
+            aux=(aux>>28)&0x0F;
         }
         i+=aux;
-        j++;
         //ArrayFunc[i-1];    EJECUTOR DE LAS FUNCIONES
         printf("%d\n",i);
     }     
     return 0;
 }
-
-
-void LeeArch(int arreglo[]){
+void LeeArch(int memoria[],int registro[]){
     FILE* arch;
     arch=fopen("traducido.mv1","rb");
     int i=0;
     if (arch == NULL)
         printf("Error en la apertura. Es posible que el archivo no exista");
-    fread(arreglo+i,sizeof(int),1,arch);
+    fread(memoria+i,sizeof(int),1,arch);
     while(!feof(arch)){
         i++;
-        fread(arreglo+i,sizeof(int),1,arch);
+        fread(memoria+i,sizeof(int),1,arch);
     }
+    registro[0]=i;
     fclose(arch);
+}
+
+void ExtraerDosOperandos(int aux,int* A,int* B){
+    if (aux>>26 & 0x03==0){//Inmediato
+        *A=aux>>12 & 0x0FFF;
+    }
+    else
+        if (aux>>26 & 0x03==2){//Directo
+            *A=get_value(A,EXTENDED_MASK);
+        }
+        else{//Registro
+        }
+    
+}
+
+int get_value(int *a, int mask){
+    int result = *a & mask;
+    if((mask&0xFF)==0){
+        result=result>>8;
+    }
+    return result;
 }
