12f2fac80147406c46df70f3d44f8ccf8b6fe79c
tobiasmdp <tobias.g@gmail.com>
2022-04-15 20:25:35 -0300

    ordenado de archivos

--- traductor/test.asm
+++ traductor/test.asm
@@ -4,7 +4,7 @@ otro: cmp [20], 100 ;compara
 jp fin ;salta si llegó a 100 o más
 swap [10], [20]
 add [20], [10]
-mov eax, %001
+mov eax, #2634234
 mov ecx, 1
 mov edx, 10
 sys 2 ;print [10] en decimal
