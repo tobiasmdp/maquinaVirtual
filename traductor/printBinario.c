44f3c01c3cfa7fa39699f387c20251af90cf98a5
Tobias <tobiasmdp13@gmail.com>
2022-04-16 18:47:21 -0300

    printeo andandndo y tmb el printBinario

--- traductor/printBinario.c
+++ traductor/printBinario.c
@@ -6,10 +6,9 @@ int main(int argc, char const *argv[])
     FILE* archB;
     int instruccion;
     printf("   DECIMAL    |    HEXA\n");
-    if ((archB = fopen(argv[2], "r")) != NULL){
+    if ((archB = fopen(argv[2], "rb")) != NULL){
         fread(&instruccion,sizeof(instruccion), 1, archB);
-        //while (!feof(archB)){ 
-        for(int i=0; i<22;i++){
+        while (!feof(archB)){ 
             printf("%15d %02X %02X %02X %02X\n", instruccion,(instruccion>>24)&0xFF, (instruccion>>16)&0xFF, (instruccion>>8)&0xFF, (instruccion)&0xFF);
             fread(&instruccion,sizeof(int), 1, archB);
         }
