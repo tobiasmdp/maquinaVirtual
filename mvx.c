d6e7f61eb142a938a3ac6a8cc81018058054e803
tobiasmdp <tobias.g@gmail.com>
2022-05-29 22:19:36 -0300

    testeado el estado y info del disco, resta lectura y escritura

--- mvx.c
+++ mvx.c
@@ -153,7 +153,7 @@ void LeeArch(int memoria[],int registro[]){
         i++;
         fread(header+i,sizeof(int),1,arch);
     }
-    
+    //el header[0] deberia valer 1297493298
    if((header[0]>>24&0x0FF)!='M' || (header[0]>>16&0x0FF)!='V' || (header[0]>>8&0x0FF)!='-' || (header[0]&0x0FF)!='2' || (header[5]>>24&0x0FF)!='V' || (header[5]>>16&0x0FF)!='.' || (header[5]>>8&0x0FF)!='2' || (header[5]&0x0FF)!='2') {
         printf("El formato del archivo .mv2 no pudo ser validado");
         exit(EXIT_FAILURE);
@@ -529,7 +529,7 @@ void SYS(int *A,int mascaraA,int *B,int C,int D,int mascaraB,int memoria[],int r
         numCil=get_value(&registro[ECX],HIGH_MASK);
         numCab=get_value(&registro[ECX],LOW_MASK);
         numSec=get_value(&registro[EDX],HIGH_MASK);
-        numDisco=get_value(&registro[EDX],LOW_MASK);
+        numDisco=get_value(&registro[EDX],LOW_MASK)-1;
         
         arch=fopen((discos+numDisco)->nombreArch,"rb");
         if (arch == NULL){
@@ -951,11 +951,11 @@ void CreaDisco(int i, int j){
     fwrite(&aux,sizeof(aux),1,arch);
     aux=0x08050301;//hora creacion
     fwrite(&aux,sizeof(aux),1,arch);
-    aux=128;//cantidad de sectores
+    aux=25;//cantidad de sectores
     aux<<=8;
-    aux|=128;//cantidad de cabezas
+    aux|=24;//cantidad de cabezas
     aux<<=8;
-    aux|=128;//cantidad de cilindros
+    aux|=23;//cantidad de cilindros
     aux<<=8;
     aux|=1;//tipo
     fwrite(&aux,sizeof(aux),1,arch);
@@ -965,7 +965,6 @@ void CreaDisco(int i, int j){
     fwrite(&aux,sizeof(int),118,arch);
     fclose(arch);
     discos[j]=discoaux;
-    
 }
 
 void LeeDiscos(){
@@ -978,8 +977,10 @@ void LeeDiscos(){
     discos= (Tdisco*)malloc(CantDiscos*sizeof(Tdisco));
     while (j<CantDiscos && j<255){
         arch=fopen(_argv[i],"rb");
-        if (arch==NULL)
+        if (arch==NULL){
             CreaDisco(i,j);
+            arch=fopen(_argv[i],"rb");
+        }
         (discos+j)->estado=0;
         fseek(arch,32,SEEK_SET);
         fread(&aux,sizeof(int),1,arch);//El menos significativo es el primero en leer
